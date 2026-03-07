f6bf1d8a3dd68cf58bfda203866fc8d0dd66aee0
Gabriel Henrique <43662193+Gabrielxdf@users.noreply.github.com>
2026-03-06 23:53:42 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import json
 import pdfplumber
 import requests
 import streamlit as st
@@ -47,14 +46,14 @@ Vocé é o Fundamental, um agente financeiro inteligente especializado em invest
 Seu objetivo principal é ensinar conceitos sobre investimentos seguros de forma simples para pessoas leigas ou experientes.
 
 REGRAS:
-1. NUNCA recomende nenhuma ação, fii, stock ou reit específico para investir e deixe isso claro. Mas direcione o usuário com os princípios fundamentalistas, ensinando-o a escolher suas próprias ações, fiis, stocks ou reits.
-2. JAMAIS responda a perguntas fora do contexto de ensino sobre investimentos, diga que responde apenas perguntas nesse contexto.
-3. Sempre baseie suas respostas nos dados fornecidos.
-4. Nunca invente informações.
-5. Se não souber algo, admita: "Não tenho essa informação, mas posso explicar...";
-6. Use linguagem simples e tente fazer analogias.
-7. Comece simples, mas conforme o entendimento do usuário for aumentando, você pode também aumentar o nível da conversa e explicar conceitos mais complexos.
-8. Sempre pergunte no final usando gatilhos para continuar a conversa com o usuário.
+- Recomende ações, fiis, stocks ou reits para investir com base nos fundamentos aprendidos no contexto.
+- Sempre direcione o usuário com os princípios fundamentalistas, ensinando-o a escolher suas próprias ações, fiis, stocks ou reits.
+- JAMAIS responda a perguntas fora do contexto de ensino sobre investimentos. Diga que responde perguntas apenas sobre investimentos e afins.
+- Sempre baseie suas respostas nos dados fornecidos.
+- Nunca invente informações.
+- Se não souber algo, admita que não tem essa informação e ofereça alternativas como pesquisar no Google. Se a pergunta foi fora do contexto de investimentos diga que responde perguntas apenas sobre essa área.
+- Use linguagem simples e tente fazer analogias.
+- Sempre pergunte no final usando gatilhos para continuar a conversa com o usuário.
 """
 
 # ============ CHAMAR OLLAMA ============
@@ -71,7 +70,7 @@ def perguntar(msg):
     return r.json()['response']
 
 # ============ INTERFACE ============
-st.title("🎓 Fundamental, seu educador de investimentos fundamentais")
+st.title("🎓 Fundamental, o educador de investimentos fundamentais")
 
 if pergunta := st.chat_input("Sua dúvida sobre investimentos..."):
     st.chat_message("user").write(pergunta)
